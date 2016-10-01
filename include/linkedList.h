1c2f1ddd39779f1cefeab2d0f45418ca3f850f27
Hugo Klepsch <hugo.klepsch@gmail.com>
2016-09-30 22:35:28 -0400

    implemented several base functionality methods

--- include/linkedList.h
+++ include/linkedList.h
@@ -1,4 +1,4 @@
-
+#include <stdbool.h>
 
 typedef struct LinkedListBody_s{
     void * data;
@@ -27,12 +27,16 @@ int length_s(LinkedList_s * head);
 
 void * getData_s(LinkedList_s * head, unsigned int index);
 
-int removeNode_s(LinkedList_s * head, unsigned int index);
+int removeNode_s(LinkedList_s * head, unsigned int index, bool freeData);
 
-int removeNodeStart_s(LinkedList_s * head);
+int removeNodeStart_s(LinkedList_s * head, bool freeData);
 
-int removeNodeEnd_s(LinkedList_s * head);
+int removeNodeEnd_s(LinkedList_s * head, bool freeData);
 
 int setData_s(LinkedList_s * head, void * data, unsigned int index);
 
-LinkedListBody_s * getElementTraverse(LinkedList_s * head, unsigned int index);
+LinkedListBody_s * getElementTraverse_s(LinkedList_s * head, unsigned int index);
+
+int destroyList_s(LinkedList_s * head, bool freeData);
+
+LinkedListBody_s * allocNode_s(void * data);
--- src/linkedList.c
+++ src/linkedList.c
@@ -12,35 +12,167 @@ int initList_s(LinkedList_s * head) {
 }
 
 int addNodeStart_s(LinkedList_s * head, void * data) {
+    LinkedListBody_s * temp;
+    if (head == NULL) {
+        return 0; //0 = fail
+    }
 
+    temp = allocNode_s(data);
+    if (temp == NULL) {
+        return 0; //failed to alloc
+    }
+
+    temp->next = head->start; //the old start comes after this new start
+    head->start = temp; //point to the new start
+    
+    return 1; //1 = success
 }
 
-int addNodeInsert_s(LinkedList_s * head, void * data, unsigned int index);
+int addNodeInsert_s(LinkedList_s * head, void * data, unsigned int index) {
+    LinkedListBody_s * temp;
+    LinkedListBody_s * leftBorderNode;
+    if (head == NULL) {
+        return 0; //0 = fail
+    }
 
-int addNodeEnd_s(LinkedList_s * head, void * data);
+    temp = allocNode_s(data);
+    if (temp == NULL) {
+        return 0; //failed to alloc
+    }
+
+    //get the node on the border
+    leftBorderNode = getElementTraverse_s(head, index); 
+    if (leftBorderNode == NULL) {
+        return 0; //end is not properly allocated
+    }
+    
+    //"insert" inbetween leftBorderNode and leftBorderNode->next
+    temp->next = leftBorderNode->next;
+    leftBorderNode->next = temp;
+
+    return 1; //all good
+}
+
+int addNodeEnd_s(LinkedList_s * head, void * data) {
+    LinkedListBody_s * temp;
+    LinkedListBody_s * oldEnd;
+    if (head == NULL) {
+        return 0; //0 = fail
+    }
+
+    temp = allocNode_s(data);
+    if (temp == NULL) {
+        return 0; //failed to alloc
+    }
+
+    //get the node on the end
+    oldEnd = getElementTraverse_s(head, length_s(head) - 1); //-1 because 0 based index vs len
+    if (oldEnd == NULL) {
+        return 0; //end is not properly allocated
+    }
+    
+    //"insert" inbetween oldEnd and oldEnd->next
+    temp->next = oldEnd->next;
+    oldEnd->next = temp;
+
+    return 1; //all good
+}
 
 int addNodeInsertionSorted_s(LinkedList_s * head, void * data, int (*compare)(void * a, void * b));
 
 int sortList_s(LinkedList_s * head, int (*compare)(void * a, void * b));
 
-int length_s(LinkedList_s * head);
+int length_s(LinkedList_s * head) {
+    LinkedListBody_s * temp;
+    int length;
 
-void * getData_s(LinkedList_s * head, unsigned int index);
+    if (head == NULL) {
+        return -1; //if the head is not allocated properly, return error
+    }
 
-int removeNode_s(LinkedList_s * head, unsigned int index);
+    length = 0;
+    if (head->start == NULL) {
+        return 0; //if there isn't a first node, return 0 length
+    }
 
-int removeNodeStart_s(LinkedList_s * head);
+    temp = head->start;
+    ++length; //go to the first node and count it
+    while (temp->next != NULL) { //traverse and count
+        temp = temp->next;
+        ++length;
+    }
 
-int removeNodeEnd_s(LinkedList_s * head);
+    return length; //once done traversing, return the count
+}
+
+void * getData_s(LinkedList_s * head, unsigned int index) {
+    LinkedListBody_s * temp;
+    if (head == NULL) {
+        return NULL; 
+    }
+
+    temp = getElementTraverse_s(head, index);
+
+    if (temp == NULL) {
+        return NULL;
+    }
+
+    return temp->data;
+}
+
+int removeNode_s(LinkedList_s * head, unsigned int index, bool freeData);
+
+int removeNodeStart_s(LinkedList_s * head, bool freeData);
+
+int removeNodeEnd_s(LinkedList_s * head, bool freeData);
 
 int setData_s(LinkedList_s * head, void * data, unsigned int index);
 
-LinkedListBody_s * getElementTraverse(LinkedList_s * head, unsigned int index) {
+LinkedListBody_s * getElementTraverse_s(LinkedList_s * head, unsigned int index) {
     LinkedListBody_s * temp;
+    int currentInd;
 
     if(head == NULL) {
         return NULL;
     }
-    
 
+    temp = head->start;
+    currentInd = 0;
+
+    //traverse until we are at the correct index
+    while(temp != NULL) {
+        if (currentInd == index) {
+            //if we're at the right index, return the current node
+            return temp;
+        }
+        temp = temp->next;
+        ++currentInd;
+    }
+    return NULL; //returns NULL on fail
 }
+
+int destroyList_s(LinkedList_s * head, bool freeData);
+
+LinkedListBody_s * allocNode_s(void * data) {
+    LinkedListBody_s * temp;
+
+    temp = malloc(sizeof(*temp));
+    if (temp == NULL) {
+        return NULL;
+    }
+    
+    temp->data = data;
+
+    return temp;
+}
+
+
+
+
+
+
+
+
+
+
+
